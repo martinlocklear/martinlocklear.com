8d8d25c763c60a7d4d4051cedc3e35ea7f4967f7
Martin Locklear <martinlocklear@users.noreply.github.com>
2020-04-02 17:19:35 -0400

    Create simple Gatsby site
    Just recreated the simple one in Gatsby to get something started.

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: "public_web_assets/martin_profile_photo_senegal.jpg"
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => 
+<div>
+    <p>Hello! This Martin's personal site. Coming soon (eventually)!</p>
+    <p>In the mean time, check out my <a href="https://www.linkedin.com/in/martinlocklear/">LinkedIn</a>.</p>
+</div>
