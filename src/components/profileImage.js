5d39327d0145ecf3d9a71ab5a8270548e4e95663
Martin Locklear <martinlocklear@users.noreply.github.com>
2020-04-19 19:04:30 -0400

    Build a profile unit to add to the main page

---
+++ src/components/profileImage.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./profileImage.css"
+
+export default () => (
+<div>
+    <img alt="Martin Locklear's lovely face" src="public_web_assets/martin_profile_photo_senegal.jpg"/>
+</div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from "react"
+import ProfileImage from "../components/profileImage"
 
-export default () => 
+export default () => (
 <div>
+    <ProfileImage />
     <p>Hello! This Martin's personal site. Coming soon (eventually)!</p>
     <p>In the mean time, check out my <a href="https://www.linkedin.com/in/martinlocklear/">LinkedIn</a>.</p>
 </div>
+)
