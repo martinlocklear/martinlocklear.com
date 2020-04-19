3d8e2c767111880a3e99b4ff5856dac2bcaf440c
Martin Locklear <martinlocklear@users.noreply.github.com>
2020-04-19 18:44:30 -0400

    Configure manifest and cleanup default configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,22 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  /* Your site config here */  
+  siteMetadata: {
+    title: `Martin Locklear`,
+    siteUrl: `https://www.martinlocklear.com`,
+    description: `Martin Locklear's Website`,
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "public_web_assets/martin_profile_photo_senegal.jpg"
+        name: `Martin Locklear's Website`,
+        short_name: `MartinLocklear.com`,
+        description: `Just a show and tell site for Martin Locklear`,
+        lang: `en`,
+        icon: `static/public_web_assets/martin_profile_photo_senegal.jpg`
       },
-    },
+    }
   ],
 }
